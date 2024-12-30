fa28ecea5bcabfa425f16a40058fe9656f676560
saurabh-hs <shshinde@mitaoe.ac.in>
2024-12-30 10:27:16 +0530

    first commit

---
+++ MyFirstProject/MyFirstProject/src/main/java/com/saurabh/MyFirstProject/MyClass.java
@@ -0,0 +1,13 @@
+package com.saurabh.MyFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+
+    @GetMapping("abc")
+    public String sayHello() {
+        return "Hello";
+    }
+}
